3ccb491331cc9bf0c2cf28844ad91c36d48e0ff6
ChenmingLi <17858975663@163.com>
2018-12-20 16:04:58 +0800

    utils文件处理
    utils文件处理

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,4 @@
-// const A = require('../vwx/uset.js');
+import A from '../vwx/common/simple.js';
 const formatTime = date => {
     const year = date.getFullYear()
     const month = date.getMonth() + 1
